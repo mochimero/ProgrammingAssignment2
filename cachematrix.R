db667000cb513f8996138d28d2abfdb169f5cace
Alfonso Barrios <el_mochimero@yahoo.com>
2016-04-21 23:53:16 -0300

    homework hopefully done...

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,16 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  xinv<-NULL ##sets default value for inverse
+  set<-function(y){
+    xinv<<-NULL
+    x<<-y
+  }
+  get<-function() {x}
+  setInverse<-function(inver){xinv<<-inver}
+  getInverse<-function() {xinv}
+  isInverted<-function() {FALSE}
+  list(set=set,get=get,setInverse=setInverse,getInverse=getInverse)
 
 }
 
@@ -11,5 +21,26 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  xinv <- x$getInverse()
+  data <- x$get()
+  
+  ##check if oldx matrix exists. It is created the first time you run call the function
+  if(!is.null(r <- get0("oldx", envir = environment()))){
+    ##checks if the input matrix has changed or not
+    if( isTRUE(all.equal(oldx,data))) {
+      print("getting cached data")
+      ##return(oldxinv)
+      return(oldxinv)
+    }
+    
+  }
+  ##the actual matrix inversion!!
+  xinv <- solve(data)
+  x$setInverse(xinv)
+  ##keeps the old INPUT matrix for checking next time the function is called
+  oldx<<-x$get()
+  ##keeps the inverse matrix cached
+  oldxinv<<-xinv
+  xinv
 }
+
